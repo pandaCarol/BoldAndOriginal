ed3b23315e3cd0b3291bce7afbc087434a821882
panda <panda.chen.2021@gmail.com>
2023-03-22 00:57:03 +0000

    removed top=0 and z-index=3 from HomeWrapper because navibar had been hidden by homepage, navibar z-index=2

--- src/app/homepage.js
+++ src/app/homepage.js
@@ -17,8 +17,6 @@ const HomeWrapper = styled(defaultSection)`
     height: 100vh;
     scroll-margin-top: 7.75rem;
     position: relative;
-    top: 0;
-    z-index: 3;
 `
 export const Homepage = () => {
 
